9228b17bb0b27ac8f3f70654c50f7b007c0e7d1c
Jonathan Rivest <jonathan_romero@hotmail.fr>
2021-03-12 13:32:06 -0500

    Début labo 4

--- themes/theme-4w4/header.php
+++ themes/theme-4w4/header.php
@@ -44,7 +44,30 @@
 				<p class="site-description"><?php echo $theme_4w4_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
+       <section class="carousel">
+	   <div>1</div>
+	   <div>2</div>
+	   <div>3</div>
+	   
+	   </section>
+	   <button id="un">1</button>
+	   <button id="deux">2</button>
+	   <button id="trois">3</button>
 
+	   <script>
+	   let bout1 = document.getElementById("un");
+	   let bout2 = document.getElementById("deux");
+	   let bout3 = document.getElementById("trois");
+	   bout1.addEventListener('mousedown', function() {
+
+	   })
+	   bout2.addEventListener('mousedown', function() {
+		   
+		})
+		bout3.addEventListener('mousedown', function() {
+		   
+		})
+	   </script>
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000000" width="32" height="32"></button>
 			<?php
